3e9b72911018f4a834d2bbf849492f36e4c88e6d
S1049546 <s2312390@bi.no>
2023-11-15 17:28:21 +0100

    added new version of project

--- 4152 project/final_exam-kopi/fullyConNN.py
+++ 4152 project/final_exam-kopi/fullyConNN.py
@@ -3,18 +3,53 @@ from  tensorflow.keras import  layers
 from  tensorflow.keras.models  import  Sequential
 
 class FullyConNN(NeuralNetwork):
+    """
+    Class FullyConNN
+    A subclass of NeuralNetwork that implements a fully connected neural network (FullyConNN).
+
+    This class creates a fully connected neural network using the TensorFlow Keras API. It provides 
+    functionalities to set up hidden layers and a classifier for the network.
+
+    Public methods:
+
+    1. __repr__():
+        nRepresents the FullyConNN instance as a string. Prints out then name of the class.
+    """
     def __init__(self):
+        """
+        Initializes the FullyConNN object.
+        This constructor initializes the FullyConNN by setting up the hidden layers, 
+        classifier, and parameters of the neural network.
+        """
         super().__init__()
-        self.set_hidden(self.hidden_layers())
-        self.set_cls(self.classifier())
+        self.set_hidden(self._hidden_layers())
+        self.set_cls(self._classifier())
         self.set_params(self._cls.trainable_variables + self._hidden.trainable_variables)
 
     def __repr__(self):
+        """
+        Represents the FullyConNN instance as a string. Prints out then name of the class.
+        """
         return print("<Fully Connencted NeuralNetwork>")
 
-    def hidden_layers(self, input_shape = 28*28, neurons = 50):
+    def _hidden_layers(self, input_shape = 28*28, neurons = 50):
+        """
+        Creates the hidden layers of the FullyConNN.
+
+        This method constructs the hidden layers using dense layers. The parameters for 
+        the layers such as the input shape and the number of neurons can be specified.
+
+        @param input_shape: The number of features in the input data. Default 28*28
+        @type input_shape: int
+
+        @param neurons: The number of neurons in each dense layer. Default 50
+        @type neurons: int
+
+        @return: The constructed hidden layers of the FullyConNN.
+        @rtype: Sequential
+        """
         hidden = Sequential([layers.InputLayer(input_shape=input_shape)
                 , layers.Dense(neurons)
                 , layers.Dense(neurons)])
-        self._last_layer_neurons = neurons
+        self._neurons = neurons
         return hidden
--- 4152 project/final_exam-kopi/train.py
+++ 4152 project/final_exam-kopi/train.py
@@ -1,4 +1,7 @@
 import argparse
+import textwrap
+import sys
+import tensorflow as tf
 
 from neuralnetwork import NeuralNetwork
 from fullyConNN import FullyConNN
@@ -9,66 +12,124 @@ from cifar import CIFAR10
 from mnist import MNIST
 
 def train_test():
-    parser = argparse.ArgumentParser(prog = "Animal test program", 
+    classes = {"neuralnetwork" : NeuralNetwork, "fully_con" : FullyConNN, "conv" : ConvNN, "dataloader" : DataLoader, "cifar10" : CIFAR10, "mnist" : MNIST}
+    parser = argparse.ArgumentParser(prog = "NeuralNetwork test program", 
                                     formatter_class = argparse.RawDescriptionHelpFormatter, 
                                     description = textwrap.dedent('''\
+                                    Classes:
+
                                                 NeuralNetwork
                                     --------------------------------
-                                    A program that creates animals and greets for each animal
-                                    
-                                    Methods:
-                                    1) getName:
-                                    @return name the name of the Animal
-                                    
-                                    2) 
-                                    Each type of animal has its own way of greeting.
+                                                FullyConNN
+                                    --------------------------------
+                                                ConvNN
+                                    --------------------------------
+                                                DataLoader
+                                    --------------------------------
+                                                MNIST
+                                    --------------------------------
+                                                CIFAR10
+                                    --------------------------------
+
+                                    To see the public interface of each class, pass inn argument:
+                                    -- doc <classname>
+                                 
                                     '''),
                                     epilog=textwrap.dedent('''\
                                                 Usage
                                     --------------------------------
-                                    # Initializes objects of different animals
-                                    e = Animal("Esteban")           
-                                    b = Dog("Bence")
-                                    k = BigDog("Karl")
-                                    c = Cat("Cleo")
+                                    optimizer = tf.keras.optimizers.Adam(learning_rate=5e-4)
+                                    #tr_data = (dset.x_tr, dset.y_tr)
+                                    tr_data = dset.loader(batch_size)
 
-                                    print(e.getName())
-                                    print("Expected: Esteban")
+                                    step =0
+                                    while step < args.epochs:
+                                        for i, data_batch in enumerate(tr_data):
+                                            losses = model.train(data_batch, optimizer)
+                                        step +=1
 
-                                    b.greet()
-                                    print("Expected: Woof")
+                                    y_and_pi = model.test(dset.x_te)
+                                    y_te = y_and_pi[0]
+                                    pi_hat = y_and_pi[1]
+                                    data_loader = dset
 
-                                    k.greet()
-                                    print("Expected Woof\nWoooof")
-
-                                    c.greet()
-                                    print("Expected: Meow")
+                                    from sklearn.metrics import roc_auc_score 
+                                    auc = roc_auc_score(data_loader.y_te, pi_hat, multi_class='ovr')
+                                    #auc = roc_auc_score(data_loader.y_te, pi_hat, multi_class='ovo')
+                                    print ('final auc %0.4f' % (auc))
+                                    
 
                                     '''))
-    parser.add_argument('--run_test', action='store_true', help='runs NeuralNetwork test')
-    #parser.add_argument('--nn_type', action='store_true', default= FullyConNN, help='Specifies what kind of Neuralnetwork to use.')
-    type = parser.add_argument('--nn_type', type = str, choices = ["FullyConNN", "ConvNN"], help='Specifies what kind of Neuralnetwork to use.')
-    epochs = parser.add_argument('--epochs', type = int, default = 10, help='Specifies the number of epochs.')
-    neurons = parser.add_argument('--neurons', type = int, default = 50, help='Specifies the number of neurons.')
-    batch_size = parser.add_argument('--batch_size', type = int, default = 256, help='Specifies the size of the batch size.')
-    dset = parser.add_argument('--dset', type = str, choices = ["MNIST", "CIFAR10"], help='Specifies which data set to use')
+    parser.add_argument('--nn_type', type = str, choices = ["fully_con", "conv"], help='Specifies the Neuralnetwork.')
+    parser.add_argument('--epochs', type = int, default = 10, help='Specifies the number of epochs.')
+    parser.add_argument('--neurons', type = int, default = 50, help='Specifies the number of neurons.')
+    parser.add_argument('--batch_size', type = int, default = 256, help='Specifies the size of the batch size.')
+    parser.add_argument('--dset', type = str, choices = ["mnist", "cifar10"], help='Specifies which data set to use')
+    parser.add_argument('--doc', type = str, choices = classes.keys(), help='Choose the doctring you want to see: --doc <name>')
 
 
-    args = parser.parse_args()
-    if args.run_test:
-        try:
-            assert isinstance(type, str)
-            assert isinstance(epochs, int)
-            assert isinstance(epochs, int)
+    args = parser.parse_args()        
+    model = args.nn_type
+    epochs = args.epochs
+    neurons = args.neurons
+    batch_size = args.batch_size
+    dset = args.dset
+    doc = args.doc
 
-            if type == "FullyCoNN":
-                type = FullyConNN()
-            elif type == "ConvNN":
-                pass
+    try:
+        #Print the doc string:
+        if doc:
+            the_class = classes[args.doc]
+            print(the_class.__doc__)
 
-
+        assert isinstance(model, str)
+        assert isinstance(epochs, int)
+        assert isinstance(neurons, int)
+        assert isinstance(batch_size, int)
+        assert isinstance(dset, str)
         
-        except AssertionError as e:
-            print(e)
+        # Check The Type of neural network:
+        if model == "fully_con":
+            model = FullyConNN()
+        elif model == "conv":
+            model = ConvNN()
+        else:
+            print("You did not specify the Neural Network class correctly")
+            sys.exit()
+        
+        # Check The data set
+        if dset == "mnist":
+            dset = MNIST()
+        elif dset == "cifar10":
+            dset = CIFAR10()
+        else:
+            print("You did not specify the DataLoader class correctly")
+            sys.exit()
 
+        optimizer = tf.keras.optimizers.Adam(learning_rate=5e-4)
+        #tr_data = (dset.x_tr, dset.y_tr)
+        tr_data = dset.loader(batch_size)
+
+        step =0
+        while step < args.epochs:
+            for i, data_batch in enumerate(tr_data):
+                losses = model.train(data_batch, optimizer)
+            step +=1
+
+        y_and_pi = model.test(dset.x_te)
+        y_te = y_and_pi[0]
+        pi_hat = y_and_pi[1]
+        data_loader = dset
+
+        from sklearn.metrics import roc_auc_score 
+        auc = roc_auc_score(data_loader.y_te, pi_hat, multi_class='ovr')
+        #auc = roc_auc_score(data_loader.y_te, pi_hat, multi_class='ovo')
+        print ('final auc %0.4f' % (auc))               
+
+    except AssertionError as e:
+        print(e)
+        print("Assertion error. One of the input values was not correct format")
+        sys.exit()
+
+train_test()
 
